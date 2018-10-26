ac44267b1dc86369019bddd420d0e0355880a682
Conrad Großer <grosserconrad@gmail.com>
2018-10-26 09:01:31 +0200

    Added some draft for the sqrt

--- radix/sqrt.c
+++ radix/sqrt.c
@@ -1,6 +1,18 @@
 // Programm zur Wurzelberechnung nach Heron
-int squareroot(int);
+unsigned long int squareroot(unsigned long int val);
 
-int main() {
-	printf("Wurzel␣aus␣%lu␣ist␣%lu", (unsigned long)81, squareroot((unsigned long)81) );
+// NO LIBARIES, LOOPS, GLOBAL VARIABLES, ONLY LONG INT
+
+// Heron Test (REMOVE BEFORE DEPLOYMENT)
+int printf(const char *, ...);
+
+int main (){
+	printf("Wurzel aus %lu ist %lu",(unsigned long)81, squareroot((unsigned long)81)); return 0;
+}
+
+// Heron Squareroot Function
+unsigned long int squareroot(unsigned long int val) {
+	int a = (val + 1) / 2;
+	int c = 0.5 * a + val/a;
+	return c;
 }
