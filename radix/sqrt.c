7f9b37cab302ad287ad2a29714def1dbbba502c4
Conrad Großer <grosserconrad@gmail.com>
2018-11-01 19:58:38 +0100

    Update radix/sqrt.c

--- radix/sqrt.c
+++ radix/sqrt.c
@@ -7,12 +7,16 @@ unsigned long int squareroot(unsigned long int val);
 int printf(const char *, ...);
 
 int main (){
-	printf("Wurzel aus %lu ist %lu",(unsigned long)81, squareroot((unsigned long)81)); return 0;
+	printf("Wurzel aus %lu ist %lu",(unsigned long)9, squareroot((unsigned long)9)); return 0;
+}
+
+unsigned long int iterator(unsigned long int val, unsigned long int a) {
+	return 0.5 * (a + val / a);
 }
 
 // Heron Squareroot Function
 unsigned long int squareroot(unsigned long int val) {
-	int a = (val + 1) / 2;
-	int c = 0.5 * a + val/a;
-	return c;
+	int startwert = val + 1 / 2;
+
+	return iterator(val, startwert);
 }
