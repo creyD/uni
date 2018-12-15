3d2e705146c7aa95e219b8cdce86e56652813498
Conrad Großer <grosserconrad@gmail.com>
2018-12-15 01:10:59 +0100

    Charset Assignment

--- charSet/charset.c
+++ charSet/charset.c
@@ -1,15 +1,15 @@
 #include "stdaup.h"
 #include "charset.h"
 
-int main(int argc, char const *argv[])
-{
-	printf("%d\n", argc);
-	charset_t *charset = charset_new(*argv);
-	free(charset);
-	return 0;
+int main() {
+charset_t *s1 = charset_new("Hallo"); charset_t *s2 = charset_new("ABCabc"); char *str1 = charset_tos(s1); charset_op(s1,s2,CS_UNION);
+char *str2 = charset_tos(s1);
+printf("str1=\"%s\"\n",str1); // ergibt: "aHlo" 
+printf("str2=\"%s\"\n",str2); // ergibt: "AaBbCcHlo"
+free(s1); free(s2); free(str1); free(str2); return 0;
 }
 
-// Länge eines Strings
+// Laenge eines Strings
 int stringLength(const char * string, unsigned long int length){
 	if(string[length] != 0){
 		return stringLength(string, ++length);
@@ -20,8 +20,10 @@ int stringLength(const char * string, unsigned long int length){
 // Exponentialfunktion
 int expon(int base, int exp){
 	int c = 1;
-	for (int i = 0; i < exp; i++){
-		c *= base;
+	if (exp != 0){
+		for (int i = 0; i < exp; i++){
+			c *= base;
+		}
 	}
 	return c;
 }
@@ -31,22 +33,22 @@ charset_t *charset_new(const char* elements){
 	int inputLaenge = stringLength(elements, 0);
 
 	// Gesamten input einlesen und jedes Zeichen speichern, wenn es ein valides Zeichen ist
-	for (int i = 0; i <= inputLaenge; i++){
-		if ((elements[i] >= 65 && elements[i] <= 72) || (elements[i] >= 97 && elements[i] <= 122)){
-			int j = 0;
-			// Großbuchstaben 65 - 90
-			if (elements[i] >= 65 && elements[i] <= 72){
-				j = elements[i] - 65;
-			}
+	for (int i = 0; i <= inputLaenge - 1; i++){
+		int j = -1;
+		if (elements[i] >= 65 && elements[i] <= 90){
 			// Kleinbuchstaben 97 - 122
-			else if (elements[i] >= 97 && elements[i] <= 122){
-				j = elements[i] - 97 + 26;
-			}
+			j = elements[i] - 65;
+		}
+		else if (elements[i] >= 97 && elements[i] <= 122){
+			// Kleinbuchstaben 97 - 122
+			j = elements[i] - 71;
+		}
+		if (j != -1){
 			// Finde das Sektion/ das Byte heraus (0-7)
 			int section = j / 8;
 			// Finde die Position/ das Bit heraus (0-7)
 			int position = j % 8;
-			new_charset->bits[section] = new_charset->bits[section] | expon(2, 8 - position);
+			new_charset->bits[section] = new_charset->bits[section] | expon(2, 7 - position);
 		}
 	}
 	return new_charset;
@@ -54,33 +56,45 @@ charset_t *charset_new(const char* elements){
 
 int charset_op(charset_t* s1, const charset_t *s2, csopt_t op){
 	switch (op){
-		// Mengen vereinigen (jedes Zeichen) übernehmen
+		// Mengen vereinigen -> alle Zeichen uebernehmen
 		case CS_UNION:
-			for (int i = 0; i < 7; i++){
+			for (int i = 0; i < 8; i++){
 				s1->bits[i] = s1->bits[i] | s2->bits[i];
 			}
 			break;
-		// Schnittmenge erstellen (nur wenn das Zeichen in beiden ist wird es übernommen)
+		// Schnittmenge erstellen -> Wenn Zeichen nur in beiden Menge wird es gespeichert
 		case CS_CUT:
-			for (int i = 0; i < 7; i++){
-				// Wenn Element nicht in beiden Mengen ist, dann 0 setzen
-				if (!(s1->bits[i] && s2->bits[i])){
-					s1->bits[i] = 0;
-				}
+			for (int i = 0; i < 8; i++){
+				s1->bits[i] = s1->bits[i] & s2->bits[i];
 			}
 			break;
+		// Symetrische Differenz berechnen -> Wenn Zeichen nur in einer Menge wird es gespeichert
 		case CS_SYMDIFF:
-			for (int i = 0; i < 7; i++){
-				// Wenn Element nicht in beiden Mengen ist, dann 0 setzen
-				if (s1->bits[i] && s2->bits[i]){
-					s1->bits[i] = 0;
-				}else{
-					s1->bits[i] = s1->bits[i] | s2->bits[i];
-				}
+			for (int i = 0; i < 8; i++){
+				s1->bits[i] = s1->bits[i] ^ s2->bits[i];
 			}
 			break;
 		default:
 			return -1;
 	}
 	return 0;
+}
+
+char *charset_tos(const charset_t* s){
+	char *string = (char *) malloc(7);
+	int iterator = 0;
+	for (int i = 0; i < 7; i++){
+		for (int j = 0; j < 8; j++){
+			if (s->bits[i] & expon(2, 7 - j)){
+				if ((i * 8) + j + 65 <= 90){
+					string[iterator] = (i * 8) + j + 65;
+				}
+				else{
+					string[iterator] = (i * 8) + j + 71;
+				}
+				iterator++;
+			}
+		}
+	}
+	return string;
 }
--- charSet/charset.h
+++ charSet/charset.h
@@ -9,4 +9,4 @@ typedef enum { CS_UNION,  // Vereinigung
 
 charset_t *charset_new(const char* elements);
 int charset_op(charset_t* s1, const charset_t *s2, csopt_t op);
-char charset_tos(const charset_t* s);
+char *charset_tos(const charset_t* s);
