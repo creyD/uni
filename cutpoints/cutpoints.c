46c7a6dab5154b87a83a68cd7e2277547fd42f73
Conrad Großer <grosserconrad@gmail.com>
2019-01-11 11:46:10 +0100

    First Cutpoints Files

---
+++ cutpoints/cutpoints.c
@@ -0,0 +1,17 @@
+#include "cp.h"
+#include "stdaup.h"
+#include "math.h"
+
+point_t *cutpoints(sprite_t sprite_a, sprite_t shape_b, int *num){
+	if (*num > 0){
+		return array;
+	}else{
+		return NULL;
+	}
+}
+
+int main()
+{
+
+	return 0;
+}
