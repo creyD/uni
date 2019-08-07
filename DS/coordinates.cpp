4469ef665a2cf1f4d29979d994162342449551ab
Conrad <grosserconrad@gmail.com>
2019-08-07 11:58:01 +0200

    Overloaded Constructor and Another
    - Added Getters and Setters for Coordinate class
    - Overloaded constructor to avoid errors when initializing empty coordinates
    - Overloaded assign operator

--- DS/coordinates.cpp
+++ DS/coordinates.cpp
@@ -9,17 +9,58 @@ class Coordinate {
   double z;
 
 public:
+  Coordinate(){
+    x = y = z = 0;
+  }
+
   Coordinate(double x_input, double y_input, double z_input){
     x = x_input;
     y = y_input;
     z = z_input;
   }
 
+  double getX(){
+    return x;
+  }
+
+  double getY(){
+    return y;
+  }
+
+  double getZ(){
+    return z;
+  }
+
+  void setX(double a){
+    x = a;
+  }
+
+  void setY(double a){
+    y = a;
+  }
+
+  void setZ(double a){
+    z = a;
+  }
+
+  void setX(int a){
+    x = (double) a;
+  }
+
+  void setY(int a){
+    y = (double) a;
+  }
+
+  void setZ(int a){
+    z = (double) a;
+  }
+
   bool isOrigin(){
     if (x == 0 && y == 0 && z == 0){
       return true;
+    } else {
+      return false;
     }
-    return false;
   }
 
   void print(){
@@ -36,19 +77,29 @@ public:
     return sqrt(pow(x - point2.x, 2) + pow(y - point2.y, 2) + pow(z - point2.z, 2));
   }
 
-  bool operator==(const Coordinate point2){
+  bool operator==(Coordinate point2){
     if (x == point2.x && y == point2.y && z == point2.z){
       return true;
     } else {
       return false;
     }
   }
+
+   void operator=(Coordinate point2){
+    x = point2.x;
+    y = point2.y;
+    z = point2.z;
+    return;
+  }
 };
 
 int main(){
   // Testing the data structure
-  Coordinate* test1 = new Coordinate(5,1,2);
+  Coordinate* test1 = new Coordinate();
+  (*test1).print();
   Coordinate* test2 = new Coordinate(5,1,2);
+  (*test1) = (*test2);
+  (*test1).print();
   cout << (*test1).distance(*test2) << endl;
   if (*test1 == *test2){
     cout << "Yay" << endl;
